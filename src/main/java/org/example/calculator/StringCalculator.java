1626edcc96ccd706cf480f4ec64de0125a08176a
elacymerys <ela.cymerys@gmail.com>
2023-02-20 14:16:40 +0100

    Add newline as separator

--- src/main/java/org/example/calculator/StringCalculator.java
+++ src/main/java/org/example/calculator/StringCalculator.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 public class StringCalculator implements Calculator {
 
     public int add(String numbers) {
-        return Stream.of(numbers.split(","))
+        return Stream.of(numbers.split(",|\n"))
                 .filter(s -> !s.isEmpty())
                 .mapToInt(Integer::parseInt)
                 .sum();
